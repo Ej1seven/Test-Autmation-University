8a267934c26429bc2355e626929d16f642c367d6
Ej1seven <erikhunter@erikhunter.dev>
2023-02-15 08:15:47 -0600

    Added AddNumbers class

---
+++ src/main/java/JavaProgramming/LetterSearch.java
@@ -0,0 +1,28 @@
+package JavaProgramming;
+
+import com.sun.tools.jdeprscan.scan.Scan;
+
+import java.util.Scanner;
+
+public class LetterSearch {
+
+    public static void main(String[] args) {
+
+        //Get text
+        System.out.println("Enter some text");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        scanner.close();
+
+        boolean letterFound = false;
+
+        //Search text for the letter A
+        for(int i = 0; i < text.length(); i++) {
+            char currentLetter = text.charAt(i);
+            if(currentLetter == 'A' || currentLetter == 'a'){
+                letterFound = true;
+                break;
+            }
+        }
+    }
+}
